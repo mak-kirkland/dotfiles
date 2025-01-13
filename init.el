2022885a5b559794ba63f057ed997f0eba27ba9e
Michael Kirkland <mak.kirkland@proton.me>
2025-01-13 21:19:21 +0100

    Re-instate `package-initialize` for when we run Emacs in batch mode

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(unless package--initialized (package-initialize))
 
 ;;;; USE-PACKAGE ;;;;
 
