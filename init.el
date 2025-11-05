2a7d2a873c7da413a86d6b3770d4c0e3dbbcf749
Michael Kirkland <mak.kirkland@proton.me>
2025-11-05 15:07:03 +0000

    feat(init): Add `tree-sitter` for syntax (grammar) highlighting

--- init.el
+++ init.el
@@ -159,6 +159,19 @@
 (use-package prettier
   :ensure t)
 
+;;;; Tree-Sitter for syntax (grammar) highlighting ;;;;
+(use-package tree-sitter
+  :ensure t
+  :config
+  (global-tree-sitter-mode 1)
+  ;; Enable highlighting
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+;; Helper package to install/manage language grammars
+(use-package tree-sitter-langs
+  :ensure t
+  :after tree-sitter)
+
 ;;;; SLY ;;;;
 (use-package sly
   :defer t
